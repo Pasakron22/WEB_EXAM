8c3abe1c8eb1f87f19a32fbcfcfa4ecfdb272f3c
Pasakron22 <31273553+Pasakron22@users.noreply.github.com>
2017-10-21 16:27:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './Pasakronnn.js'; 
+import Header from './Pasakronnn.js';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom'; 
+import routes from './routing-config';
+
+
 class App extends Component {
   constructor() {
     super();
@@ -13,6 +17,8 @@ class App extends Component {
   }
   render() {
     const myname = "Pasakron";
+    const dUrl = "https://mysafeinfo.com/api/data?list=presidents&format=json";
+    console.log(dUrl);
     return (
       <div className="App">
         <header className="App-header">
@@ -23,6 +29,19 @@ class App extends Component {
           <Header />
         </p>
       </div>
+      <BrowserRouter>
+        <div>
+          <h1 className="left" >WEB_EXAM</h1>
+          <ul>
+            <li><NavLink to="/" >Home</NavLink></li>
+            <li><NavLink to="/president" >president</NavLink></li>
+          </ul>
+          
+            {routes.map((route,index) => (<Route key={index} path={route.path} component={route.component} exact={route.exact} />))}  
+         
+          
+        </div>
+      </BrowserRouter>
 
     );
   }
